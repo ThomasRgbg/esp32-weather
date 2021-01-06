436b933b038d8a878365b789227cd7af16714064
ThomasRgbg <ThomasRgbg@users.noreply.github.com>
2021-01-06 12:16:56 +0100

    inital checkin

---
+++ boot.py
@@ -0,0 +1,17 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import gc
+import webrepl
+
+import network
+webrepl.start()
+gc.collect()
+
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True) 
+wlan.scan()
+wlan.connect('AAAAA', 'BBBB')
+wlan.ifconfig()
+
